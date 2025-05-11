699786d9f84a4b4e570d5aa0b9472a37b420c8b5
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2025-05-11 10:14:42 -0400

    Review and Dealership microservice

--- server/database/app.js
+++ server/database/app.js
@@ -11,8 +11,8 @@ app.use(require('body-parser').urlencoded({ extended: false }));
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-const dbUser = process.env.MONGO_USER;  // Access MONGO_HOST variable
-const dbPass = process.env.MONGO_PASS;  // Access MONGO_PORT variable
+const dbUser = process.env.MONGO_USER;  
+const dbPass = process.env.MONGO_PASS;
 
 mongoose.connect(`mongodb://${dbUser}:${dbPass}@mongo_db:27017/`, {
     'dbName': 'dealershipsDB',
