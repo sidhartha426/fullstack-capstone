34deaeec3bc6010141c3bb4cb6fb6b398ef18566
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2025-05-12 08:58:59 -0400

    Add dealership dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
-        path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -22,7 +22,7 @@ const Dealers = () => {
     const retobj = await res.json();
     if(retobj.status === 200) {
       let state_dealers = Array.from(retobj.dealers)
-      setDealersList(state_dealers)
+      setDealersList(state_dealers);
     }
   }
 
@@ -35,11 +35,11 @@ const Dealers = () => {
       let all_dealers = Array.from(retobj.dealers)
       let states = [];
       all_dealers.forEach((dealer)=>{
-        states.push(dealer.state)
+        states.push(dealer.state);
       });
 
-      setStates(Array.from(new Set(states)))
-      setDealersList(all_dealers)
+      setStates(Array.from(new Set(states)).sort());
+      setDealersList(all_dealers);
     }
   }
   useEffect(() => {
