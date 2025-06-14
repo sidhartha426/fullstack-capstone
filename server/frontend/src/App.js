8895c35ff28090e9affab351f36113c8876832a4
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2025-06-14 07:48:01 -0400

    Car Searching

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPanel from "./components/Login/Login";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview"
+import SearchCars from "./components/Dealers/SearchCars";	
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/searchcars/:id" element={<SearchCars />} />
     </Routes>
   );
 }
